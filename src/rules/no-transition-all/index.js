d619ad9401e3a777ae9fd441e50354a9637cf629
ksenius <30226190+ksenius@users.noreply.github.com>
2021-08-10 22:02:07 +0700

    Improve no-transition-all rule

--- src/rules/no-transition-all/index.js
+++ src/rules/no-transition-all/index.js
@@ -4,8 +4,23 @@ const namespace = require('../../utils/namespace');
 const ruleName = namespace('no-transition-all');
 
 const messages = utils.ruleMessages(ruleName, {
-  rejected: 'The keyword "all" should not be used with the "transition" property',
+  rejected: 'The keyword "all" should not be used with the "transition" property explicitly or implicitly',
 });
+
+const isImplicitTransitionAll = (shorthandValue) => {
+  const valueParts = shorthandValue.split(',').map(value => value.trim().split(' '));
+
+  const TIMING_FUNCTION_REGEXP = /^(ease|linear|step|cubic-bezier)/;
+  const PROPERTY_REGEXP = /^[a-z]+(-[a-z]+)?$/;
+
+  for (let i = 0; i < valueParts.length; i += 1) {
+    if (!valueParts[i].some(value => PROPERTY_REGEXP.test(value) && !TIMING_FUNCTION_REGEXP.test(value))) {
+      return true;
+    }
+  }
+
+  return false;
+};
 
 const rule = actual => (root, result) => {
   const validOptions = utils.validateOptions(result, ruleName, { actual });
@@ -15,9 +30,10 @@ const rule = actual => (root, result) => {
   }
 
   root.walkDecls(/transition/, (decl) => {
-    const valueArr = decl.value.split(' ');
+    const isExplicitAll = () => decl.value.split(',').flatMap(val => val.trim().split(' ')).some(val => val === 'all');
+    const isImplicitAll = () => decl.prop === 'transition' && isImplicitTransitionAll(decl.value);
 
-    if (valueArr.find(val => val === 'all')) {
+    if (isExplicitAll() || isImplicitAll()) {
       utils.report({
         message: messages.rejected,
         node: decl,
